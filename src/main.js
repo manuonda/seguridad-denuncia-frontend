5555b76fd9e0bc6bf643ff78c7a73bff106647e6
david garcia <manuonda@gmail.com>
2019-06-17 23:32:34 -0300

    Se agrega la pantalla de Denuncia Robo

--- src/main.js
+++ src/main.js
@@ -17,7 +17,21 @@ Vue.use(VueRouter);
 Vue.use(VueScrollTo);
 Vue.use(BootstrapVue);
 
-
+/*
+jQuery.extend(true, jQuery.fn.datetimepicker.defaults, {
+  icons: {
+    time: 'far fa-clock',
+    date: 'far fa-calendar',
+    up: 'fas fa-arrow-up',
+    down: 'fas fa-arrow-down',
+    previous: 'fas fa-chevron-left',
+    next: 'fas fa-chevron-right',
+    today: 'fas fa-calendar-check',
+    clear: 'far fa-trash-alt',
+    close: 'far fa-times-circle'
+  }
+});
+*/
 const enrutador = new VueRouter({
    routes : routes,
    mode: 'history'
